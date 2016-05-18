61e06530e400b4becb3ce76276ce4b1707e14f09
YunjiaoWang <dxjiao@gmail.com>
2016-05-18 11:48:16 -0500

    Using hierarchical model to analysis our data

---
+++ Data_Analysis/HierarchicalModel_Ratios.R
@@ -0,0 +1,73 @@
+# This function defines and runs the model
+# The data is not standardized
+genMCMC.hierarchical = function( data , xName="x" , yName="y" , sName="s" ,
+                                 numSavedSteps=50000 , thinSteps = 1 , 
+                                 saveName=NULL , runjagsMethod=runjagsMethodDefault ,
+                                 nChains=nChainsDefault) 
+{ 
+  require(rjags)
+  #-----------------------------------------------------------------------------
+  # THE DATA.
+  # THE DATA.
+  y = data[,yName]
+  x = data[,xName]
+  s = as.numeric(factor(data[,sName]))
+  Ntotal = length(y)
+  Nsubj = length(unique(s))
+  # Specify the data in a list, for later shipment to JAGS:
+  dataList = list(
+    x = x ,
+    y = y ,
+    s = s,
+    Ntotal = Ntotal ,
+    Nsubj = Nsubj  # should equal length(unique(s))
+  )
+  
+  # THE MODEL.
+  # Specify the model for unstandardized data:
+  modelString = "
+  model {
+  for ( i in 1:Ntotal ) {
+  y[i] ~ dt(  beta0[s[i]] + beta1[s[i]] * x[i], 1/sigma^2, 4 )
+  }
+  for ( s in 1:Nsubj ) {
+      beta0[s] ~ dnorm( beta0mu , 1/(beta0sigma)^2 )  
+      beta1[s] ~ dnorm( beta1mu , 1/(beta1sigma)^2 )
+    }
+  # Priors vague 
+  beta0mu ~ dnorm( 0 , 1/(10)^2 )
+  beta1mu ~ dnorm( 0 , 1/(10)^2 )
+  sigma ~ dunif( 1.0E-3 , 1.0E+3 )
+  beta0sigma ~ dunif( 1.0E-3 , 1.0E+2 )
+  beta1sigma ~ dunif( 1.0E-3 , 1.0E+2 )
+  }
+  " # close quote for modelString
+  # Write out modelString to a text file
+  writeLines( modelString , con="TEMPmodel.txt" )
+  #-----------------------------------------------------------------------------
+  # INTIALIZE THE CHAINS.
+  # Let JAGS do it...
+  #-----------------------------------------------------------------------------
+  # RUN THE CHAINS
+  parameters = c( "beta0" ,  "beta1" , "beta0mu" , "beta1mu" , "beta0sigma" , "beta1sigma" )
+  
+  adaptSteps = 2000  # Number of steps to "tune" the samplers
+  burnInSteps = 2000
+  runJagsOut <- run.jags( method=runjagsMethod ,
+                          model="TEMPmodel.txt" , 
+                          monitor=parameters , 
+                          data=dataList ,  
+                          #inits=initsList , 
+                          n.chains=nChains ,
+                          adapt=adaptSteps ,
+                          burnin=burnInSteps , 
+                          sample=ceiling(numSavedSteps/nChains) ,
+                          thin=thinSteps ,
+                          summarise=FALSE ,
+                          plots=FALSE )
+  codaSamples = as.mcmc.list( runJagsOut )
+  # resulting codaSamples object has these indices: 
+  #   codaSamples[[ chainIdx ]][ stepIdx , paramIdx ]
+  
+  return( codaSamples )
+} # end function
---
+++ Data_Analysis/ModelTransitionMean_hierarchical.R
@@ -0,0 +1,68 @@
+# This function defines and runs the model
+# The data is not standardized
+genMCMC_trans = function( data,  yName="y", sName = "s",
+                    numSavedSteps=50000 , saveName=NULL ) { 
+  require(rjags)
+  #-----------------------------------------------------------------------------
+  # THE DATA.
+  y = data[,yName]
+ # x = data[,xName]
+  s = data[,sName]
+  
+  # Specify the data in a list, for later shipment to JAGS:
+  dataList = list(
+    #x = x ,
+    y = y ,
+    s = s ,
+    Ntotal = length(y),
+    Nsubj = length(unique(s))
+  )
+  #-----------------------------------------------------------------------------
+  # THE MODEL.
+  # Specify the model for unstandardized data:
+  modelString = "
+  model {
+  for ( i in 1:Ntotal ) {
+  y[i] ~ dbern( theta0[s[i]] )
+  }
+  for ( s in 1:Nsubj ) {
+      # Use the mode (omega) and concentration (kappa) parmaters for the beta
+      theta0[s] ~ dbeta(omega*(kappa-2)+1,(1-omega)*(kappa-2)+1)  
+    }
+  # Priors vague 
+  omega ~ dbeta(1,1)
+  kappa <- kappaMinusTwo + 2
+  kappaMinusTwo ~ dgamma(0.01,0.01)
+  }
+  " # close quote for modelString
+  # Write out modelString to a text file
+  writeLines( modelString , con="TEMPmodel.txt" )
+  #-----------------------------------------------------------------------------
+  # INTIALIZE THE CHAINS.
+  # Let JAGS do it...
+  #-----------------------------------------------------------------------------
+  # RUN THE CHAINS
+  parameters = c( "theta0" )
+  adaptSteps = 500  # Number of steps to "tune" the samplers
+  burnInSteps = 1000
+  nChains = 4 
+  thinSteps = 1
+  nIter = 5000
+  #nIter = ceiling( ( numSavedSteps * thinSteps ) / nChains )
+  # Create, initialize, and adapt the model:
+  jagsModel = jags.model( "TEMPmodel.txt" , data=dataList , #inits=initsList , 
+                          n.chains=nChains , n.adapt=adaptSteps )
+  # Burn-in:
+  cat( "Burning in the MCMC chain...\n" )
+  update( jagsModel , n.iter=burnInSteps )
+  # The saved MCMC chain:
+  cat( "Sampling final MCMC chain...\n" )
+  codaSamples = coda.samples( jagsModel , variable.names=parameters , 
+                              n.iter=nIter , thin=thinSteps )
+  # resulting codaSamples object has these indices: 
+  #   codaSamples[[ chainIdx ]][ stepIdx , paramIdx ]
+  if ( !is.null(saveName) ) {
+    save( codaSamples , file=paste(saveName,"Mcmc.Rdata",sep="") )
+  }
+  return( codaSamples )
+} # end function
